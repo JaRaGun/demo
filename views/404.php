b8e5eecff11dbc72b35775d9a24ddbc7dff06b62
janrafael.guno <guno.148983@pasay-edsa.sti.edu.ph>
2023-12-23 18:10:56 +0800

    first commit

---
+++ views/404.php
@@ -0,0 +1,12 @@
+<?php   require ('partials/head.php') ?>
+<?php require('partials/nav.php')?>
+
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <p class="text-2xl text-blue-900">
+                Sorry, Page not found
+            </p>
+            <a class="text-grey-900 underline" href="/">Go back to homepage</a>
+        </div>
+    </main>
+<?php //require ('partials/footer.php')?>
